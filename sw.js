431db114b9400384d011defdbf97ecc3cb70e7cc
To0mo <hamitwamitomo230@gmail.com>
2026-03-15 16:20:39 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-const CACHE_NAME = 'code-pro-mirror';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './manifest.json'
-];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
